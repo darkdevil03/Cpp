241f1ebb0d07e3e86580b976cf7727f0fc0d0463
darkdevil03 <bvvsatyanarayana280301@gmail.com>
2026-07-10 15:05:39 +0530

    implemented the identifier concept.

---
+++ basics-of-cpp/Ex06_Identifiers.cpp
@@ -0,0 +1,20 @@
+/*
+    The general rules for naming variables are:
+        Names can contain letters, digits and underscores
+        Names must begin with a letter or an underscore (_)
+        Names are case-sensitive (myVar and myvar are different variables)
+        Names cannot contain whitespaces or special characters like !, #, %, etc.
+        Reserved words (like C++ keywords, such as int) cannot be used as names
+ */
+
+#include <iostream>
+using namespace std;
+int main() {
+    // Good
+    int minutesPerHour = 60;
+    cout << "Minutes per hour : " << minutesPerHour << endl;
+
+    // OK, but not so easy to understand what m actually is
+    int m = 60;
+    cout << "Minutes per hour : " << m << endl;
+}
