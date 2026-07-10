66bf65263e1512b9d47005a4caa9cb501d623a73
darkdevil03 <bvvsatyanarayana280301@gmail.com>
2026-07-10 14:26:47 +0530

    implemented single and multi  line comments syntax.

---
+++ basics-of-cpp/Ex03_Comments.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hello there!" << "\n"; // Single-line comment
+    cout << "Done single-line and multi-line comments syntax in code";
+    /* multi-line comments
+       like this in cpp
+     */
+}
