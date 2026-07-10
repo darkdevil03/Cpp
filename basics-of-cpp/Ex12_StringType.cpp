b6c869c423665030e8b891412d7eb15e7b9deb7b
darkdevil03 <bvvsatyanarayana280301@gmail.com>
2026-07-10 15:51:27 +0530

    implemented the string data types input concept.

---
+++ basics-of-cpp/Ex12_StringType.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main () {
+    cout << "String Data Type " << endl;
+    // Include the string library
+
+
+    // Create a string variable
+    string greeting = "Hello";
+
+    // Output string value
+    cout << greeting;
+}
