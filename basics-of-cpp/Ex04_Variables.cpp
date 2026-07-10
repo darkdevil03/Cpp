0a689a131cf290fb904b94fd1fa9a6142f4483e9
darkdevil03 <bvvsatyanarayana280301@gmail.com>
2026-07-10 14:39:41 +0530

    added variables program.

---
+++ basics-of-cpp/Ex04_Variables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int myNum = 5;               // Integer (whole number without decimals)
+    cout << " int : " << myNum << "\n";
+    double myFloatNum = 5.99;    // Floating point number (with decimals)
+    cout << " double : " << myFloatNum << "\n";
+    char myLetter = 'D';         // Character
+    cout << " char : " << myLetter<< "\n";
+    string myText = "Hello";     // String (text)
+    cout << " String : " << myText << "\n";
+    bool myBoolean = true;       // Boolean (true or false)
+    cout << " bool : " << myBoolean << endl;
+
+    return 0;
+}
