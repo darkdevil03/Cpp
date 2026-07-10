1c73f54607d96c81b7ac45ef9a6d041e082c62ca
darkdevil03 <bvvsatyanarayana280301@gmail.com>
2026-07-10 14:19:54 +0530

    added concept from line 4 to 7.

--- basics-of-cpp/Ex02_NewLines.cpp
+++ basics-of-cpp/Ex02_NewLines.cpp
@@ -1,12 +1,13 @@
-//
-// Created by mrdev on 10-07-2026.
-//
-
 #include <iostream>
 using namespace std;
 int main () {
+    /* The newline character (\n) is called an escape sequence,
+     * and it forces the cursor to change its position to the beginning of the next line on the screen.
+     * This results in a new line.
+     */
     cout << "Hi, \n";
-    cout << "Welcome to cpp. \n";
+    cout << "Welcome to cpp." << "\n";
+    cout << "Hello there!";
 
     return 0;
 }
