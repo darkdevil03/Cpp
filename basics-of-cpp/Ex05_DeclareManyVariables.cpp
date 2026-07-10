0d8dcd3fece1ecf0dc12eabcf3a3897c54a9412c
darkdevil03 <bvvsatyanarayana280301@gmail.com>
2026-07-10 14:59:29 +0530

    added one value to multpile variables.

--- basics-of-cpp/Ex05_DeclareManyVariables.cpp
+++ basics-of-cpp/Ex05_DeclareManyVariables.cpp
@@ -2,6 +2,13 @@
 #include <iostream>
 using namespace std;
 int main() {
+    // To declare more than one variable of the same type, use a comma-separated list:
     int a = 10, b = 20, c = 30;
     cout << "sum of a, b, and c : " << a + b + c << endl;
+
+    // We can also assign the same value to multiple variables in one line:
+    int x, y, z;
+    x = y = z = 50;
+    cout << "sum of x, y, and z : "<< x + y + z;
+
 }
