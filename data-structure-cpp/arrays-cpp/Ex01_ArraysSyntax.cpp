fea109a1cad6043253c72d96d2c92704c1e16ff9
darkdevil03 <bvvsatyanarayana280301@gmail.com>
2026-07-14 15:50:11 +0530

    For Ex01_ArraysSyntax.cpp, Arrays implemented.

---
+++ data-structure-cpp/arrays-cpp/Ex01_ArraysSyntax.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    /*
+       Arrays
+        Arrays are used to store multiple values in a single variable, instead of declaring separate variables for each value.
+        To declare an array, define the variable type,
+        specify the name of the array followed by square brackets and specify the number of elements it should store
+     */
+
+    // We have now declared a variable that holds an array of four strings.
+    // To insert values to it, we can use an array literal - place the values in a comma-separated list, inside curly braces:
+    string programming[4] = {"C", "C++","Java","Python"};
+
+    // Printing using for loop
+    for (int i = 0; i < size(programming) ; i++) {
+        cout << programming[i] << "\t";
+    }
+
+    // Printing using for-each loop
+    for (string s : programming) {
+        cout << s << endl;
+    }
+
+    // We can also print with other loops also.
+    // We know that when range known to print for loop is the best choice.
+
+    return 0;
+}
