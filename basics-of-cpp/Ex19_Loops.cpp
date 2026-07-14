b8102b58f09d1bf876af7ff1a893e2fa8ad5df69
darkdevil03 <bvvsatyanarayana280301@gmail.com>
2026-07-14 14:47:36 +0530

    For Ex18_SwitchStatement.cpp, do while implemented.

--- basics-of-cpp/Ex19_Loops.cpp
+++ basics-of-cpp/Ex19_Loops.cpp
@@ -26,5 +26,23 @@ int main() {
         start++;
     }
 
+    /*
+    2. The Do/While Loop
+        The do/while loop is a variant of the while loop.
+        This loop will execute the code block once, before checking if the condition is true.
+        Then it will repeat the loop as long as the condition is true.
+        The example below uses a do/while loop. The loop will always be executed at least once,
+        even if the condition is false, because the code block is executed before the condition is tested.
+    */
+    cout << "------------------------------------------" << endl;
+    cout << "|            2. Do While Loop            |" << endl;
+    cout << "------------------------------------------" << endl;
+
+    cout << "Start = "<< start << endl;
+    do {
+        cout << start << "" << endl;
+        start--;
+    }while (start > 0); // Note: The semicolon ; after the while condition is required!
+    cout << "Happy New Year!" << endl;
     return 0;
 }
