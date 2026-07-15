964dd23a8ba2c52f5debba4ed274b03cfe6aed12
darkdevil03 <bvvsatyanarayana280301@gmail.com>
2026-07-15 11:59:23 +0530

    For Ex01_ArraysSyntax.cpp, added note point at line 10.

--- data-structure-cpp/arrays-cpp/Ex01_ArraysSyntax.cpp
+++ data-structure-cpp/arrays-cpp/Ex01_ArraysSyntax.cpp
@@ -7,7 +7,8 @@ int main() {
         Arrays are used to store multiple values in a single variable, instead of declaring separate variables for each value.
         To declare an array, define the variable type,
         specify the name of the array followed by square brackets and specify the number of elements it should store
-     */
+    Note: This is because the size of an array in C++ is fixed, meaning we cannot add or remove elements after it is created.
+    */
 
     // We have now declared a variable that holds an array of four strings.
     // To insert values to it, we can use an array literal - place the values in a comma-separated list, inside curly braces:
