e9a9123e70da88cbbb9ce4f9d296d65d2c035c0d
darkdevil03 <bvvsatyanarayana280301@gmail.com>
2026-07-17 15:12:57 +0530

    For Ex02_FunctionalParametersAndArguments.cpp, Function's parameter and arguments passing implemented optimized and added concept.

---
+++ chapter02_functions-cpp/Ex02_FunctionalParametersAndArguments.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+/*
+    C++ Function Parameters and Arguments:
+        Information can be passed to functions as a parameter.
+        Parameters act as variables inside the function.
+        Parameters are specified after the function name, inside the parentheses.
+        We can add as many parameters as you want, just separate them with a comma:
+ */
+
+void parametersAndArguments(string input) { // string input: parameter
+    cout << "My functional parameter : " << input << endl;
+}
+
+int main () {
+    parametersAndArguments("Dev"); // "Dev" : is Argument
+    parametersAndArguments("Ram");
+
+    /*
+    When a parameter is passed to the function, it is called an argument.
+    So, from the above: input is a parameter, while "Dev" and "Ram" are arguments.
+     */
+
+    return 0;
+}
