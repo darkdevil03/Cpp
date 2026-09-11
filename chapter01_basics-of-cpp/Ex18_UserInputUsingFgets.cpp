893b01872d568667e6f3277faccaa975085ac7f9
darkdevil03 <bvvsatyanarayana280301@gmail.com>
2026-09-11 16:50:58 +0530

    Implemented another way read input from keyboard.

---
+++ chapter01_basics-of-cpp/Ex18_UserInputUsingFgets.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main() {
+    cout << "Enter your input string " << endl;
+    string input;
+    fgets(getline(cin,input,'\n'));
+
+    return 0;
+}
